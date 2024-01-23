3f76919a139c08214d77278a64946ef367b06e56
viktorholk <holkinator@gmail.com>
2024-01-23 13:10:33 +0100

    test routes

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,16 +1,21 @@
-import { Router, Request, Response } from 'express';
-import AuthMiddleware from '../middleware/auth';
+import { Router, Request, Response } from "express";
+import AuthMiddleware from "../middleware/auth";
 
-import UserRouter from './user';
-import AuthRouter from './auth';
+import UserRouter from "./user";
+import AuthRouter from "./auth";
 
 const router = Router();
 
-router.get('/timestamp', AuthMiddleware, (req: Request, res: Response) => {
+// Test Routes
+router.get("/timestamp", (req: Request, res: Response) => {
   res.send(`${new Date()}`);
 });
 
-router.use('/user', UserRouter);
-router.use('/auth', AuthRouter);
+router.get("/auth-timestamp", AuthMiddleware, (req: Request, res: Response) => {
+  res.send(`${new Date()}`);
+});
+
+router.use("/user", UserRouter);
+router.use("/auth", AuthRouter);
 
 export default router;
