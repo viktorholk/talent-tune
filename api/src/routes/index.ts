9b2923344a8c6b9c41af606c846b93affc2c2c49
viktorholk <holkinator@gmail.com>
2024-01-25 21:05:21 +0100

    optimize endpoint and openai setup

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -6,16 +6,37 @@ import AuthRouter from "./auth";
 
 import correlator from "correlation-id";
 import Logger from "../utils/logger";
+import { getStreamingCompletion } from "../helpers/assistant";
 
 const router = Router();
 
-// Test Routes
-router.get("/timestamp", (req: Request, res: Response) => {
-  res.sendResponse(200, { timestamp: `${new Date()}` });
-});
+router.post("/optimize", async (req: Request, res: Response) => {
+  const params = req.body;
 
-router.get("/auth-timestamp", AuthMiddleware, (req: Request, res: Response) => {
-  res.sendResponse(200, { timestamp: `${new Date()}` });
+  if (!params.jobDescription || !params.resume)
+    return res.sendResponse(400, "Missing parameters");
+
+  try {
+    const stream = await getStreamingCompletion(
+      params.jobDescription,
+      params.resume,
+      params.instructions
+    );
+    let starttime = Date.now();
+    for await (const part of stream) {
+      const chunkTime = (Date.now() - starttime) / 1000;
+
+      const chunk = part.choices[0]?.delta.content;
+
+      Logger.info(chunk);
+
+      res.write(chunk || "");
+    }
+
+    res.end();
+  } catch (error) {
+    res.sendResponse(500, error);
+  }
 });
 
 router.use("/user", UserRouter);
