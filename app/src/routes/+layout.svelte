d78326629cacef40ae684948c3eb808ab7c895e6
Timme Snogdal sørensen <72727621+Snogdal2@users.noreply.github.com>
2024-02-01 12:50:07 +0100

    home and layout

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+
+	import { page } from '$app/stores';
+</script>
+
+<head>
+
+</head>
+<body>
+	{#if $page.url.pathname != '/login'}
+		<nav>
+            <a href="/dashboard">Dashboard</a>
+            <a href="/company">company</a>
+            <a href="/user">user</a>
+            <a href="/login">Logout</a>
+        </nav>              
+	{/if}
+	<main>
+		<slot />
+	</main>
+</body>
