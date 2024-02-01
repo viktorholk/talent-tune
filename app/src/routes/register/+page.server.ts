9a3c13f88e0f63d39509d3eb1938be074d527eaf
Timme Snogdal sørensen <72727621+Snogdal2@users.noreply.github.com>
2024-02-01 14:42:19 +0100

    register

---
+++ app/src/routes/register/+page.server.ts
@@ -0,0 +1,35 @@
+// @ts-nocheck
+import post from '$lib/actions/loginAction';
+import { redirect } from '@sveltejs/kit';
+import { fail } from '@sveltejs/kit';
+
+export async function load({ cookies }) {
+  cookies.delete('jwt', { path: '/' });
+  return { ok: true };
+}
+
+export const actions = {
+  default: async ({ request, cookies }) => {
+    let res;
+    const formData = await request.formData();
+    const json = {};
+    // @ts-ignore
+    formData.forEach((value, key) => {
+      json[key] = value;
+    });
+    console.log(formData);
+    try {
+      res = await post('/user/create', json);
+      cookies.set('jwt', JSON.stringify({ token: res.token }), { path: '/' });
+    } catch (error) {
+      return fail(422, {
+        error: error.message
+      });
+    }
+    if (cookies.get('jwt') != null) {
+      throw redirect(303, '/');
+    } else {
+      return fail();
+    }
+  }
+};
--- app/src/routes/register/+page.svelte
+++ app/src/routes/register/+page.svelte
@@ -1,15 +1,23 @@
 <script>
 	export let form;
 </script>
-<h1>Login</h1>
+<h1>register</h1>
 <form method="POST" enctype="multipart/form-data" >
 	<label for="email">email</label>
 	<input type="text" id="email" name="email" required/>
+
+    <label for="name">name</label>
+	<input type="text" id="name" name="name" required/>
+
 	<label for="password">password</label>
 	<input type="password" id="password" name="password" required/>
+
+    <label for="password">password again</label>
+	<input required/>
+
 	<div class="button-group">
-		<button type="submit">Login</button>
-		<button>forgot password?</button>
+		<button type="submit">register</button>
+		<button>login</button>
 		{#if form?.error}
 			<p>{form.error}</p>
 		{/if}
