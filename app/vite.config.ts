9cd73c9f1b4d334ca3f6f64b8569563d5ba163ac
viktorholk <holkinator@gmail.com>
2024-02-01 12:51:30 +0100

    added redis mongodb and new svelte app do docker compose

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -2,8 +2,18 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  },
+  preview: {
+    port: 3000,
+    strictPort: true
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:3000'
+  }
 });
