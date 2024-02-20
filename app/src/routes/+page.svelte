6f28097ecbcaaebc2aaa9a84cd10c1c1b9ccde01
viktorholk <holkinator@gmail.com>
2024-02-20 13:55:16 +0100

    formatting. Fixed scroll to bottom on chat. Disable forms when streaming

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,99 +1,117 @@
 <script  lang="ts">
-
-import { post } from '$lib/actions/fetching';
+import {
+    post
+} from '$lib/actions/fetching';
 import SvelteMarkdown from 'svelte-markdown'
-	export let data;
+export let data;
 
-  const user = data.user;
+const user = data.user;
 
-  let id = "";
-  let  messages =  []
+let id = "";
+let messages = []
 
 
-  const handleChat = async e => {
+let streaming = false;
 
-    const formData = new FormData(e.target)
 
-    const message = formData.get('message')
+const streamCompletion = async (formData) => {
+    streaming = true
 
-    messages = [...messages, {role: "user", message: message}]
 
-      const response = await post(
-        `/assistant/chat/${id}`,
-        {
-          message
-        },
+    let payload;
+    let endpoint;
+    if (id) {
+
+        payload = {
+            message: formData.get('message')
+        }
+        endpoint = `/assistant/chat/${id}`
+    } else {
+        payload = {
+            resume: formData.get('resume'),
+            jobDescription: formData.get('jobDescription')
+        }
+
+        endpoint = "/assistant/initialize/"
+    }
+
+    const response = await post(
+        endpoint,
+        payload,
         data.token
-      );
+    );
 
-  e.target.reset();
+    if (!id) {
+      id = response.headers.get('x-assistant-id');
+    }
 
-      const messagesClone = [...messages]
+    const messagesClone = [...messages] 
 
-      let fullMessage = "";
+    let fullMessage = "";
 
-  const reader = response.body.getReader();
-const decoder = new TextDecoder();
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
+    const reader = response.body.getReader();
+    const decoder = new TextDecoder();
+    while (true) {
+        const {
+            done,
+            value
+        } = await reader.read();
+        if (done) {
+            break;
+        }
+
+
+        const decodedChunk = decoder.decode(value, {
+            stream: true
+        });
+        fullMessage += decodedChunk;
+        messages = [...messagesClone, {
+            role: "assistant",
+            message: fullMessage
+        }]
     }
 
 
-      const decodedChunk = decoder.decode(value, { stream: true });
-      fullMessage += decodedChunk;
-      messages = [...messagesClone, {role: "assistant", message: fullMessage}]
-  }
+    streaming = false;
+}
 
 
 
-  }
+const handleChat = async e => {
 
+    const formData = new FormData(e.target);
 
+    const message = formData.get('message');
 
-  const handleProcess = async e => {
-    const formData = new FormData(e.target)
-user.isCompany
-    try {
-      const response = await post(
-        '/assistant/initialize',
-        {
-          resume: formData.get('resume'),
-          jobDescription: formData.get('jobDescription')
-        },
-        data.token
-      );
+    if (message.length > 0) {
 
-      id = response.headers.get("x-assistant-id")
-      const messagesClone = [...messages]
+      messages = [...messages, {
+          role: "user",
+          message: message
+      }]
 
-      let fullMessage = "";
-
-  const reader = response.body.getReader();
-const decoder = new TextDecoder();
-  while (true) {
-    const { done, value } = await reader.read();
-    if (done) {
-      break;
+      await streamCompletion(formData);
     }
 
 
-      const decodedChunk = decoder.decode(value, { stream: true });
-      fullMessage += decodedChunk;
-      messages = [...messagesClone, {role: "assistant", message: fullMessage}]
-  }
-
-  e.target.reset();
-
-
-    } catch (error) { }
+    e.target.reset();
+}
 
 
 
-  }
-  
+const handleProcess = async e => {
+    const formData = new FormData(e.target)
 
+    id = "";
+    messages = []
+
+
+    await streamCompletion(formData);
+
+    e.target.reset();
+}  
+
+console.log(!id || streaming)
 </script> 
 
 <div class="container mx-auto px-4">
@@ -103,44 +121,35 @@ const decoder = new TextDecoder();
    <div class="flex">
       <div class="w-1/3">
          <form class="flex flex-col items-center justify-center" method="POST" enctype="multipart/form-data" on:submit|preventDefault={handleProcess}>
-            <textarea name="resume" class="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md" placeholder="Paste your resume here"></textarea>
-            <textarea name="jobDescription" class="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md" placeholder="Paste the job description here"></textarea>
-            <button type="submit"class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-full">
+            <textarea disabled={streaming} name="resume" class="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md" placeholder="Paste your resume here"></textarea>
+            <textarea  disabled={streaming} name="jobDescription" class="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md" placeholder="Paste the job description here"></textarea>
+            <button  disabled={streaming} type="submit"class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-6 rounded-full">
             Process
             </button>
          </form>
       </div>
       <div class="flex-1 px-5 gap-4">
-         <div class="flex flex-col gap-4 bg-gray-100 min-h-64 max-h-96 mb-2 overflow-y-scroll p-2">
-
-        {#each messages as { role, message}, i}
-        {#if role == "assistant"}
-         <h3 class="text-indigo-600 font-bold text-2xl">Assistant</h3>
-         <SvelteMarkdown source={message} isInline/>
-         {:else}
-         <div class="flex flex-col text-right">
-
-         <h3 class="text-indigo-600 font-bold text-xl italic">{data.user.name}</h3>
-         <p class="text-sm">{message}</p>
-         </div>
-
-        {/if}
-
-
-
-        {/each}
-
-
+         <div class="flex flex-col flex-col-reverse gap-4 bg-gray-100 min-h-64 max-h-96 mb-2 overflow-y-scroll p-2 rounded-xl">
+            {#each [...messages].reverse() as { role, message}, i}
+            {#if role == "assistant"}
+            <SvelteMarkdown source={message} isInline/>
+            <h3 class="text-indigo-600 font-bold text-2xl">Assistant</h3>
+            {:else}
+            <div class="flex flex-col text-right">
+               <p class="text-sm">{message}</p>
+               <h3 class="text-indigo-600 font-bold text-xl italic">{data.user.name}</h3>
+            </div>
+            {/if}
+            {/each}
          </div>
          <form  method="POST" enctype="multipart/form-data" on:submit|preventDefault={handleChat}>
-            <textarea disabled={!id} name="message" class="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md" placeholder="Message"></textarea>
+            <textarea disabled={!id || streaming} name="message" class="w-full h-32 p-2 mb-4 border border-gray-300 rounded-md" placeholder="Message"></textarea>
             <div class="flex justify-end">
-<button disabled={!id} class={`bg-indigo-600 hover:bg-indigo-700 font-bold py-3 px-6 rounded-full ${!id ? 'bg-gray-100 text-black' : 'text-white'}`}>
-            Chat 
-            </button>
-
+               <button disabled={!id || streaming} class={`  font-bold py-3 px-6 rounded-full ${!id || streaming ? 'bg-gray-100 text-gray-300' : 'text-white bg-indigo-600 hover:bg-indigo-700'}`}>
+               Chat 
+               </button>
             </div>
-                     </form>
+         </form>
       </div>
    </div>
    {/if}
