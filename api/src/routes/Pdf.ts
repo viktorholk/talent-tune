7830cc91de6e7e4daa440bb9e1b0f921d0721609
viktorholk <holkinator@gmail.com>
2024-03-11 23:12:29 +0100

    removed unused code

--- api/src/routes/Pdf.ts
+++
@@ -1,9 +0,0 @@
-import { Router, Request, Response } from "express";
-
-import exportTextFromPDF from "../helpers/pdf-handler";
-
-const router = Router();
-
-router.post("/PDF", exportTextFromPDF);
-
-export default router;
