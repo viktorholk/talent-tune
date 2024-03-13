015192f2e19b0a79006fd515d001380936f5b189
viktorholk <holkinator@gmail.com>
2024-03-13 16:35:20 +0100

    Fixed search disappearing on company browse page

--- app/src/lib/components/company/Browse.svelte
+++ app/src/lib/components/company/Browse.svelte
@@ -46,35 +46,34 @@
 </script>
 
 <h1 class="text-center text-2xl text-indigo-700 font-bold">Browse Potential applicants</h1>
+<div class="mx-auto flex items-center rounded border border-gray-300 p-1 w-96">
+  <svg
+    class="w-4 h-4 text-gray-400 ml-1"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 20"
+  >
+    <path
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+    />
+  </svg>
+  <input
+    type="search"
+    class="w-full px-2 focus:outline-none text-md border-none bg-transparent"
+    placeholder="Search Name, Biography and Tags"
+    bind:value={searchFilter}
+  />
+</div>
 
 {#if $profilesStore?.length === 0}
-  <p class="text-center">...</p>
+  <p class="text-center">No applicants found</p>
 {:else}
   <div class="flex flex-col justify-center items-center mt-5 gap-3">
-    <div class="flex items-center rounded border border-gray-300 p-1 w-96">
-      <svg
-        class="w-4 h-4 text-gray-400 ml-1"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 20 20"
-      >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-        />
-      </svg>
-      <input
-        type="search"
-        class="w-full px-2 focus:outline-none text-md border-none bg-transparent"
-        placeholder="Search Name, Biography and Tags"
-        bind:value={searchFilter}
-      />
-    </div>
-
     {#each $profilesStore as profile (profile._id)}
       <a href={`/profiles/${profile._id}`}>
         <div
