b0186003d985cc4284856756c37630de64919887
viktorholk <holkinator@gmail.com>
2024-03-19 08:54:35 +0100

    added user deletion and cascade route

--- api/src/controllers/user.ts
+++ api/src/controllers/user.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 
 import UserModel from "@/models/user";
+import ProfileModel from "@/models/profile";
+import DocumentModel from "@/models/document";
+import CompanyModel from "@/models/company";
+import JobListingModel from "@/models/job-listing";
+
 import bcrypt from "bcrypt";
 import _ from "lodash";
 
@@ -18,4 +23,22 @@ export async function update(req: Request, res: Response) {
   await user.save();
 
   return res.sendResponse(200, _.omit(user.toObject(), "password"));
+}
+
+export async function del(req: Request, res: Response) {
+  const user = await UserModel.findById(req.user?._id);
+
+  if (!user) return res.sendResponse(400, "user does not exist");
+
+  // delete associated profile / company data
+  if (user.isCompany()) {
+    await CompanyModel.findByIdAndDelete(user.company?._id);
+    await JobListingModel.deleteMany({ company: user.company?._id });
+  } else {
+    await ProfileModel.findByIdAndDelete(user.profile?._id);
+    await DocumentModel.deleteMany({ profile: user.profile?._id });
+  }
+  await UserModel.findByIdAndDelete(user._id);
+
+  return res.sendResponse(200, "User removed");
 }
